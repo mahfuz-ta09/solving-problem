584c918151558363fa0b3250cc5e3726d7589a53
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2024-11-18 23:47:25 +0600

    Add files via upload

---
+++ w-4/d2/TwoElevators.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define no cout<<"NO\n";
+#define yes cout<<"YES\n";
+#define nl cout<<"\n"
+#define vec(x) for(auto a:x)cout<<a<<" ";cout<<"\n";
+#define srt(v) sort(v.begin(),v.end());
+#define iOS ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+void solve(){
+    ll i,j,n,m,p,ans=0;
+    cin>>n>>m>>p;
+
+    i = n-1;
+    j = abs(m-p)+p-1;
+
+    if(i<=j){
+        ans++;
+    }
+
+    if(i>=j){
+        ans+=2;
+    }
+
+
+    cout<<ans;
+
+    nl;
+}
+
+int main(){
+    iOS
+    ll t;
+    cin >> t;
+    while(t--)solve();
+    //getline(cin,ss);
+    return 0;
+}
+
+
