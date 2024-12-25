8f8b16975158a6f73aa2a93ca956b7f16e2c2c96
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2024-12-25 23:53:52 +0600

    Add files via upload

---
+++ w-9/d4/Fadi and LCM.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define no cout<<"NO\n";
+#define yes cout<<"YES\n";
+#define nl cout<<"\n"
+#define vec(x) for(auto a:x)cout<<a<<" ";cout<<"\n";
+#define srt(v) sort(v.begin(),v.end());
+#define iOS ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+void solve(){
+    ll i,j,n,m,p,ans=0;
+
+    vector<ll> v(n);
+}
+
+int main(){
+    iOS
+    ll t;
+    //t=1;
+    cin >> t;
+    while(t--)solve();
+    //getline(cin,ss);
+    return 0;
+}
+
+
