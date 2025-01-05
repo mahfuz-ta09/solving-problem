0bafd19027752ca35f26d4441adb80e58115b41b
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-01-05 20:12:00 +0600

    Add files via upload

---
+++ w10/d-8/d-8/Collecting Numbers.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define no cout<<"NO\n";
+#define yes cout<<"YES\n";
+#define nl cout<<"\n"
+#define vec(x) for(auto a:x)cout<<a<<" ";cout<<"\n";
+#define srt(v) sort(v.begin(),v.end());
+#define iOS ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+void solve(){
+    ll i,j,n,m,p,ans=1;
+
+    cin>>n;
+    ll l = 1;
+    vector<ll> v(n+1,0);
+
+    for (i=1;i<=n;i++) {
+    	ll x;
+    	cin>>x;
+    	v[x] = i;
+    }
+
+    for (int i = 1; i <= n; i++) {
+    	if(l>v[i]){
+    		ans++;
+    	}
+    	l = v[i];
+    }
+    cout<<ans;
+}
+
+int main(){
+    iOS
+    ll t;
+    t=1;
+    //cin >> t;
+    while(t--)solve();
+    //getline(cin,ss);
+    return 0;
+}
+
+
