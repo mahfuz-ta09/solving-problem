94c3a3579844c166273777e00c443380d19f2d16
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-01-20 18:27:45 +0600

    Add files via upload

---
+++ w-12/d-3/Simple Palindrome.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define no cout<<"NO\n";
+#define yes cout<<"YES\n";
+#define nl cout<<"\n"
+#define vec(x) for(auto a:x)cout<<a<<" ";cout<<"\n";
+#define srt(v) sort(v.begin(),v.end());
+#define iOS ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+void solve(){
+    ll i,j,n,m,p,ans=0;
+    cin>>n;
+
+    string a,b="aeiou";
+
+    m=n/5;
+    p=n%5;
+
+    while(m--){
+        a+=b;
+    }
+
+
+    for(i=0;i<p;i++){
+        a+=b[i];
+    }
+
+    srt(a);
+    cout<<a;
+    nl;
+}
+
+int main(){
+    iOS
+    ll t;
+    cin >> t;
+    while(t--)solve();
+    //getline(cin,ss);
+    return 0;
+}
+
+
