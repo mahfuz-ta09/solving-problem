5ec1c1b8c108eb3a1d0f9f9e96407d4c91644352
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-02-08 23:45:05 +0600

    Add files via upload

---
+++ d-1/Sakurako and Kosuke.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define no cout<<"NO\n";
+#define yes cout<<"YES\n";
+#define nl cout<<"\n"
+#define vec(x) for(auto a:x)cout<<a<<" ";cout<<"\n";
+#define srt(v) sort(v.begin(),v.end());
+#define iOS ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+void solve(){
+    ll i,j,n,m,p,ans=0;
+    cin>>n;
+
+    if(n<1){
+        n=-n;
+    }
+
+    if(n%2){
+        cout<<"Kosuke";
+    }else{
+        cout<<"Sakurako";
+    }
+    nl;
+}
+
+int main(){
+    iOS
+    ll t;
+    //t=1;
+    cin >> t;
+    while(t--)solve();
+    //getline(cin,ss);
+    return 0;
+}
+
+
