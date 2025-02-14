486720819019c6428976a4352fc7a9c7f489ea4c
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-02-14 23:37:37 +0600

    Add files via upload

---
+++ w-14/d-7/Primary Task.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define no cout<<"NO\n";
+#define yes cout<<"YES\n";
+#define nl cout<<"\n"
+#define vec(x) for(auto a:x)cout<<a<<" ";cout<<"\n";
+#define srt(v) sort(v.begin(),v.end());
+#define iOS ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+void solve(){
+    ll i,j,n,m,a,ans=0;
+    cin >> a;
+
+    if((a>=102&&a<=109)||(a>=1010&&a<=1099)) {
+        yes;
+    }else{
+        no;
+    }
+}
+
+int main(){
+    iOS
+    ll t;
+    t=1;
+    cin >> t;
+    while(t--)solve();
+    //getline(cin,ss);
+    return 0;
+}
+
+
